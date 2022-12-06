3b20c7c771faf60b6444d77b27381c0a940aa729
Novi Hardiansyah <49898975+novihardiansyah@users.noreply.github.com>
2022-12-07 06:00:32 +0700

    Create 2_extracting_html_ipynb.py

---
+++ 2_extracting_html_ipynb.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context() # ssl - Secure Sockets Layer
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# get the URL to extract HTML so parsing that with BeautifulSoup
+url = 'https://www.cia.gov/the-world-factbook/countries/china/'
+print("Opening the file connection...")
+
+uh = urllib.request.urlopen(url, context=ctx)
+print("HTTP status",uh.getcode())
+
+html = uh.read().decode()
+print(f"Reading done. Total {len(html)} characters read.", "\n")
+
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.prettify())
